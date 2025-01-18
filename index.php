019b65e702d74a77443616b8846f4f6673d9431e
danilosenati <danilosena.ti@gmail.com>
2025-01-18 00:07:25 -0300

    upload files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Help Desk</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/boots>
+
+    <style>
+        .card-login{
+            padding: 30px 0 0 0;
+            width
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    echo "oi";
+    ?>
+
+</body>
+
+</html>
