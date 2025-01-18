5e69ea8b0b17bfe4ee4384f8075dd9eb0dcf67a4
danilosenati <danilosena.ti@gmail.com>
2025-01-18 19:39:39 -0300

    upload 1.0

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>App Help Desk</title>
+    <title>Menu - Help Desk</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -17,9 +17,9 @@
   <body>
 
     <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="index.php">
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
+        Help Desk
       </a>
     </nav>
 
--- index.php
+++ index.php
@@ -4,20 +4,52 @@
 <head>
     <meta charset="utf-8" />
     <title>Help Desk</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/boots>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/boots">
 
     <style>
-        .card-login{
+        .card-login {
             padding: 30px 0 0 0;
-            width
+            width: 350px;
+            margin: 0 auto;
+        }
+
+        a {
+            text-decoration: none;
         }
     </style>
 </head>
 
 <body>
-    <?php
-    echo "oi";
-    ?>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Help Desk
+        </a>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="card-login">
+                <div class="card">
+                    <div class="card-header">
+                        Login
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="E-mail">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" placeholder="Senha">
+                            </div>
+                            <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 
 </body>
 
