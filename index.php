db547c092e1ebbb2968cb4a103ca79e69b318eb7
danilosenati <danilosena.ti@gmail.com>
2025-01-22 22:46:06 -0300

    version 1.2

--- index.php
+++ index.php
@@ -44,6 +44,16 @@
                             <div class="form-group">
                                 <input name="senha" type="password" class="form-control" placeholder="Senha">
                             </div>
+                             
+                            <!-- SE DIGITAR O USUARIO OU SENHA INVALIDO É EXIBIDO NA TELA A MENSAGEM ABAIXO:  -->
+                            <?php if(isset($_GET['login']) && $_GET['login'] == 'erro'){?>
+
+                                <div class="text-danger">
+                                Usuário ou senha inválido(s)
+                                </div>
+
+                                <?php } ?>
+
                             <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
                         </form>
                     </div>
